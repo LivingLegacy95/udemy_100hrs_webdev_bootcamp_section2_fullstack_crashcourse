acb3d32fe0e0061c8a8546014274c1ef4e58a01f
Keithin Greene <kgreene012@gmail.com>
2026-06-27 16:53:02 -0400

    installed nodemon, started working on routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ app.get("/api/notes", (req, res) => {
     res.send("you got 5 notes");
 });
 
+app.post("/api/notes", (req,res) => { 
+    res.status(201).send({message:"post has been created successfully"})
+});
+
+app.update("/api/notes", (req,res) => { 
+    res.status(20).send({message:"post has been updated successfully"})
+});
+
+
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
 });

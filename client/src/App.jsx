a833d3d9f4956b8975d81daff513847d083c2e9c
Keithin Greene <kgreene012@gmail.com>
2026-07-10 23:59:03 -0400

    Added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,14 @@ import DetailPage from './Views/DetailPage';
 const App = () => {
   return (
     <div data-theme="coffee">
-      <button className="btn btn-primary">Click me</button>
+      {/* <button className="btn btn-primary">Click me</button>
       <button className="btn">Button</button>
       <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
       <button className="btn btn-secondary">Secondary</button>
       <button className="btn btn-accent">Accent</button>
       <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <button className="btn btn-link">Link</button> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />

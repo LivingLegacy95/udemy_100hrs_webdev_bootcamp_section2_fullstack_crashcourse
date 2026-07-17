8841576cbd588663465e2df9b741681869e8f24e
Keithin Greene <kgreene012@gmail.com>
2026-07-16 23:36:50 -0400

    started detail page

---
+++ client/src/Views/NoteDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NoteDetailPage = () => {
+    const [note, setNote] = useState("null");
+    const [loading, setLoading] = useState(false);
+    return (
+        <div className="min-h-screen bg-base-200">
+            <h1>DetailPage</h1>
+        </div>
+    );
+};
+
+export default NoteDetailPage;

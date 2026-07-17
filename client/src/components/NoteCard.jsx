0dc9d2f1b3bc422868a8a104efacda3eb86a9a9d
Keithin Greene <kgreene012@gmail.com>
2026-07-17 19:31:18 -0400

    fixed rendering bug

--- client/src/components/NoteCard.jsx
+++ client/src/components/NoteCard.jsx
@@ -1,5 +1,4 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
-import React from 'react'
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils'
 import api from '../lib/axios'
@@ -9,19 +8,21 @@ const NoteCard = ({ note, setNotes }) => {
     const handleDelete = async (e, id) => {
         e.preventDefault(); // since NoteCard is a link the default behavior when clicking on delete button is to navigate to notedetail page. This method prevents that behavior.
         if (!window.confirm("Are you sure you want to delete this note?")) return;
-    }; try {
-        await api.delete(`/notes/${id}`)
-        setNotes((prev) => prev.filter(note => note._id !== id)) // removes object from array
-        toast.success("Note deleted successfully");
-    } catch (error) {
-        console.log(error, "Error in handleDelete");
-        toast.error("Failed to delete note");
-    }
+
+        try {
+            await api.delete(`/notes/${id}`);
+            setNotes((prev) => prev.filter((note) => note._id !== id)); // removes object from array
+            toast.success("Note deleted successfully");
+        } catch (error) {
+            console.log(error, "Error in handleDelete");
+            toast.error("Failed to delete note");
+        }
+    };
 
     return (
         <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
             <div className="card-body">
-                <h3 className="card=title text-base-content">{note.title}</h3>
+                <h3 className="card-title text-base-content">{note.title}</h3>
                 <p className="text-base-content/70 line-clamp-3">{note.content}</p>
                 <div className="card-actions justify-between items-center mt-4">
                     <span className="text-sm text-base-content/60">

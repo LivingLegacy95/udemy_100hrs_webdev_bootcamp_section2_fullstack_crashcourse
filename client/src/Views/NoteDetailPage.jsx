ca8648222fa5b7e0da8e42f4a5159a047f87c8cb
Keithin Greene <kgreene012@gmail.com>
2026-07-28 19:20:49 -0400

    added delete and save function to note detail

--- client/src/Views/NoteDetailPage.jsx
+++ client/src/Views/NoteDetailPage.jsx
@@ -26,7 +26,34 @@ const NoteDetailPage = () => {
 
         fetchNote();
     }, [id])
-    const handleDelete = () => { }
+    const handleDelete = async () => {
+        if (!window.confirm("Are you sure you want to delete this note?")) return;
+        try {
+            await api.delete(`/notes/${id}`);
+            toast.success("Note deleted");
+            navigate("/")
+        } catch (error) {
+            console.log("Error deleting the note", error);
+            toast.error("Failed to delete the note");
+        }
+    };
+    const handleSave = async () => {
+        if (!note.title.trim() || !note.content.trim()) {
+            toast.error("Please add a title or content");
+            return;
+        }
+        setSaving(true)
+        try {
+            await api.put(`/notes/${id}`, note)
+            toast.success("Note saved successfully ")
+            navigate("/")
+        } catch (error) {
+            console.log("Error saving the note", error);
+            toast.error("Failed to save the note");
+        } finally {
+            setSaving(false);
+        }
+    };
     if (loading) {
         return (
             <div className="min-h-screen bg-base-200 flex items-center justify-center">
@@ -75,6 +102,23 @@ const NoteDetailPage = () => {
                                     value={note.title}
                                     onChange={(e) => setNote({ ...note, title: e.target.value })}
                                 />
+                            </div>
+                            <div className="form-control mb-4">
+                                <label className="label">
+                                    <span className="label-text">Content</span>
+                                </label>
+                                <textarea
+                                    placeholder="Write your note here...."
+                                    className="textarea textarea-bordered h-32"
+                                    value={note.content}
+                                    onChange={(e) => setNote({ ...note, content: e.target.value })}
+                                />
+                            </div>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary" disabled={saving} onClick={handleSave}>
+                                    {saving ? "Saving..." : "Save Changes"}
+
+                                </button>
                             </div>
                         </div>
                     </div>
